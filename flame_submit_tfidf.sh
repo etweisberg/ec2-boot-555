abe1b97fcddc17db22cbbad44a7c1410d76a0dc1
christopherwun <ckywun123@gmail.com>
2024-12-09 22:33:25 -0500

    fixed script

--- flame_submit_tfidf.sh
+++ flame_submit_tfidf.sh
@@ -10,10 +10,9 @@ fi
 COORDINATOR="$PUB_IP:9000"
 
 # Submit Flame jobs with different seed URLs
-for seed_url in "${SEED_URLS[@]}"; do
-  echo "Submitting Flame job with seed URL: $seed_url"
-  java -cp crawler.jar:flame.jar:kvs.jar:webserver.jar:log4j_lib/*:lucene_lib/* cis5550.flame.FlameSubmit "$COORDINATOR" jobs.jar cis5550.jobs.TfIdfEC2 &
-done
+echo "Submitting Flame job: $seed_url"
+java -cp crawler.jar:flame.jar:kvs.jar:webserver.jar:log4j_lib/*:lucene_lib/* cis5550.flame.FlameSubmit "$COORDINATOR" jobs.jar cis5550.jobs.TfIdfEC2 &
+
 
 # Wait for all background processes to complete
 wait
