e82ab661eae7a51aa07845f9fd8fb7a2973a533b
Ethan Weisberg <ethaniweisberg@gmail.com>
2024-12-09 20:26:23 -0500

    worker reduction

--- flame_worker.sh
+++ flame_worker.sh
@@ -10,7 +10,7 @@ fi
 COORDINATOR="$PUB_IP:9000"
 
 # Start 100 workers
-for ((i=1; i<=100; i++)); do
+for ((i=1; i<=8; i++)); do
   PORT=$((9000 + i))
   echo "Starting Worker on port $PORT..."
   java -cp kvs.jar:flame.jar:webserver.jar:log4j_lib/* cis5550.flame.Worker $PORT $COORDINATOR &
