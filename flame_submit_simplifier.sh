cd8db5e03c38bae57ffec4f0a9a3230c80dc7c47
Ethan Weisberg <ethaniweisberg@gmail.com>
2024-12-10 13:33:29 -0500

    updated permissions + simplifier script

--- flame_submit_simplifier.sh
+++ flame_submit_simplifier.sh
@@ -10,8 +10,8 @@ fi
 COORDINATOR="$PUB_IP:9000"
 
 # Submit Flame jobs with different seed URLs
-echo "Submitting Flame job: $seed_url"
-java -Xms24g -Xmx24g -cp crawler.jar:flame.jar:kvs.jar:webserver.jar:log4j_lib/*:lucene_lib/* cis5550.flame.FlameSubmit "$COORDINATOR" jobs.jar cis5550.jobs.TfIdfEC2 &
+echo "Submitting Simplifier..."
+java -Xms24g -Xmx24g -cp flame.jar:kvs.jar:webserver.jar:log4j_lib/*:lucene_lib/* cis5550.flame.FlameSubmit "$COORDINATOR" simplifier.jar cis5550.jobs.Simplifier &
 
 
 # Wait for all background processes to complete
